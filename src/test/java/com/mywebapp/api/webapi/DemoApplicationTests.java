1974b104a998c26c03bc5d48a63d719d2667e3cf
MWANGIKENNEDY <kenmwan75@gmail.com>
2020-08-08 19:40:01 +0100

    Created the project structure

---
+++ src/test/java/com/mywebapp/api/webapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywebapp.api.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
